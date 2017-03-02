81b5028b44e8ca01776abce96ac46e9a2614888e
ian <ian@t400>
2017-03-02 02:18:32 -0400

    Added two new rules
    Added rules for the local-part and domain.
    Added tests for these new rules
    Added test for all rules
    Refactored.

--- src/main/java/com/example/email_validator/Validator.java
+++ src/main/java/com/example/email_validator/Validator.java
@@ -18,22 +18,17 @@ public class Validator {
 	 * Validates email according to a set of rules. 
 	 * 
 	 * @param email Email address to be validated.
+	 * @param rules The rules to be checked against
 	 * 
 	 * @return the number of checks that passed.
 	 */
-    public static Integer validate(String email) {
-    	Integer checksPassed = 0;
-    	String[] rules = {"[^@]*@[^@]*", ".*\\.+.*"};
+    public static Integer validate(String email, String[] rules) {
+    	Integer checksPassed = 0;  
     	for (Integer i = 0; i < rules.length; i++) {
-    		System.out.println("Checking " + rules[i]);
     		if (Pattern.matches(rules[i], email)) {
     			checksPassed++;
     		}
     	}
     	return checksPassed;
-    }
-    
-    public static void main(String[] args) {
-    	System.out.println(validate("ian.dawsondal.ca"));
     }
 }
--- src/test/java/com/example/email_validator/ValidatorTest.java
+++ src/test/java/com/example/email_validator/ValidatorTest.java
@@ -5,6 +5,8 @@ package com.example.email_validator;
 
 /**
  * This class uses junit to test the Validator class.
+ * 
+ * @author ian-dawson
  */
 
 
@@ -12,39 +14,82 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
-public class ValidatorTest {	
-	@Test
-	public void testEmptyEmail() {
-		assertTrue(Validator.validate("") == 0);
-	}
+public class ValidatorTest {
+	private String[] ruleList = {
+						"[^@]*@[^@]*",	//Exactly 1 "@"
+						".*\\.+.*",		//At least one "."
+						".+@.*",		//Local-part is present
+						".*@.+\\..+"	//Domain is present
+					};
 	
 	@Test
-	public void testEmailWithoutAtOrDot() {
-		assertTrue(Validator.validate("ian-dawson") == 0);
+	public void testEmptyEmail() {
+		String[] rules = {ruleList[0], ruleList[1]};
+		assertTrue(Validator.validate("", rules) == 0);
 	}
 	
 	@Test
 	public void testEmailWithoutDot() {
-		assertTrue(Validator.validate("ian-dawson@example") == 1);
+		String[] rules = {ruleList[1]};
+		assertTrue(Validator.validate("ian-dawson@example", rules) == 0);
+	}
+	
+	@Test
+	public void testEmailWithDot() {
+		String[] rules = {ruleList[1]};
+		assertTrue(Validator.validate("example.com", rules) == 1);
+	}
+	
+	@Test
+	public void testEmailWithMultipleDot() {
+		String[] rules = {ruleList[1]};
+		assertTrue(Validator.validate("mail.example.com", rules) == 1);
 	}
 	
 	@Test
 	public void testEmailWithoutAt() {
-		assertTrue(Validator.validate("ian-dawsonexample.com") == 1);
+		String[] rules = {ruleList[0]};
+		assertTrue(Validator.validate("mail.example.com", rules) == 0);
 	}
 	
 	@Test
-	public void testEmailWithSingleAtAndDot() {
-		assertTrue(Validator.validate("ian-dawson@example.com") == 2);
+	public void testEmailWithSingleAt() {
+		String[] rules = {ruleList[0]};
+		assertTrue(Validator.validate("ian-dawson@example", rules) == 1);
 	}
 	
 	@Test
-	public void testEmailWithMultipleAtAndDot() {
-		assertTrue(Validator.validate("ian@dawson@example.com") == 1);
+	public void testEmailWithMultipleAt() {
+		String[] rules = {ruleList[0]};
+		assertTrue(Validator.validate("ian@dawson@example", rules) == 0);
 	}
 	
 	@Test
-	public void testEmailWithSingleAtAndMultipleDot() {
-		assertTrue(Validator.validate("ian-dawson@mail.example.com") == 2);
+	public void testEmailHasNoUser() {
+		String[] rules = {ruleList[2]};
+		assertTrue(Validator.validate("@example.com", rules) == 0);
+	}
+	
+	@Test
+	public void testEmailHasUser() {
+		String[] rules = {ruleList[2]};
+		assertTrue(Validator.validate("ian-dawson@", rules) == 1);
+	}
+	
+	@Test
+	public void testEmailHasNoDomain() {
+		String[] rules = {ruleList[3]};
+		assertTrue(Validator.validate("ian-dawson@example", rules) == 0);
+	}
+	
+	@Test
+	public void testEmailHasDomain() {
+		String[] rules = {ruleList[3]};
+		assertTrue(Validator.validate("@example.com", rules) == 1);
+	}
+	
+	@Test
+	public void testAllRules() {
+		assertTrue(Validator.validate("ian-dawson@example.com", ruleList) == ruleList.length);
 	}
 }
